76a4f508e5012662852d6ff8008bb2e957dc771c
Ben Vogt <benjvogt@gmail.com>
2015-08-05 17:44:10 -0500

    working on adding markdown support for read/write operations

--- assets/js/loads.js
+++ assets/js/loads.js
@@ -4,11 +4,13 @@ requirejs.config({
       index: 'index',
       jquery: 'jquery-2.1.4.min',
       handlebars: 'handlebars-v3.0.3',
-      moment: 'moment'
+      moment: 'moment',
+      markdown: 'markdown.min'
     },
     shim: {
       'jquery': 'jquery-2.1.4.min',
       'moment': 'moment',
+      'markdown': 'markdown.min',
       'handlebars': 'handlebars-v3.0.3',
       index: {
         deps: ['jquery']
@@ -20,5 +22,6 @@ requirejs([
   'jquery',
   'handlebars',
   'moment',
+  'markdown',
   'index'
 ]);
