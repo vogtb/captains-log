b9cdcb8e6667d02dc64e9d4e3656a4a98aee6b10
Ben Vogt <benjvogt@gmail.com>
2015-09-05 10:38:25 -0500

    seperating view into react components

--- renderer/js/components.js
+++ renderer/js/components.js
@@ -4,7 +4,6 @@ define(function (require) {
     _ = require('underscore'),
     yaml = require('yaml');
 
-  var LOG_LINE_HOLDER_ID = 'log-holder';
 
   var LogLine = React.createClass({displayName: "LogLine",
     render: function () {
@@ -18,10 +17,32 @@ define(function (require) {
   });
 
   var LogBox = React.createClass({displayName: "LogBox",
+    componentDidMount: function () {
+      // //actually load data
+      // var file = ipc.sendSync('load-file', {
+      //   'directory': localStorage.directory,
+      //   'file': localStorage.file
+      // });
+    },
+    getInitialState: function() {
+      // this should be empty. mocking out data for development.
+      return {
+        data: {
+          lines: [{
+            line: "This is a line",
+            timestamp: "This is a timestamp"
+          },
+          {
+            line: "This is a line",
+            timestamp: "This is a timestamp"
+          }]
+        }
+      };
+    },
     render: function () {
       return (
         React.createElement("div", null, 
-          this.props.data.lines.map(function(line) {
+          this.state.data.lines.map(function(line) {
             return React.createElement(LogLine, {data: line});
           })
         )
@@ -29,19 +50,72 @@ define(function (require) {
     }
   });
 
-  var data = {
-    lines: [{
-      line: "This is a line",
-      timestamp: "This is a timestamp"
-    },
-    {
-      line: "This is a line",
-      timestamp: "This is a timestamp"
-    }]
-  };
-  React.render(
-    React.createElement(LogBox, {data: data}),
-    document.getElementById(LOG_LINE_HOLDER_ID)
-  );
+  var Instructions = React.createClass({displayName: "Instructions",
+    render: function () {
+      return (
+        React.createElement("div", {className: "mdl-grid hidden"}, 
+          React.createElement("div", {className: "mdl-cell mdl-cell--12-col"}, 
+            React.createElement("h5", null, "Where would you like to store the logs?"), 
+            React.createElement("button", {className: "mdl-button mdl-js-button mdl-button--raised"}, "Choose Directory")
+          )
+        )
+      );
+    }
+  });
 
+  var MainInput = React.createClass({displayName: "MainInput",
+    render: function () {
+      return (
+        React.createElement("div", {className: "mdl-textfield mdl-js-textfield"}, 
+          React.createElement("textarea", {className: "mdl-textfield__input main-input", type: "text", rows: "4"}), 
+          React.createElement("label", {className: "mdl-textfield__label", for: "main-input"}, "Log here...")
+        )
+      );
+    }
+  });
+
+  var FileName = React.createClass({displayName: "FileName",
+    render: function () {
+      return (
+        React.createElement("span", {className: "mdl-layout-title filename", id: "filename", contentEditable: "true"}, "untitlted_log_file")
+      );
+    }
+  });
+
+  var Nav = React.createClass({displayName: "Nav",
+    render: function () {
+      var logoPath = path.join(__dirname, 'img', 'icon.png');
+      return (
+        React.createElement("div", {className: "mdl-layout__header-row menu"}, 
+          React.createElement("span", null, React.createElement("img", {className: "logo", src: logoPath})), 
+          React.createElement(FileName, null), 
+          React.createElement("i", {className: "material-icons warning hidden", id: "warning"}, "warning"), 
+          React.createElement("div", {className: "mdl-layout-spacer"}), 
+          React.createElement("div", {id: "notification", className: "notification hidden"}), 
+          React.createElement("div", {className: "mdl-layout-spacer"}), 
+          React.createElement("span", {className: "hidden directory", id: "directory"}), 
+          React.createElement("button", {className: "mdl-button mdl-js-button mdl-button--icon", id: "change-directory"}, 
+            React.createElement("i", {className: "material-icons"}, "folder_open")
+          ), 
+          React.createElement("button", {className: "mdl-button mdl-js-button mdl-button--icon", id: "new"}, 
+            React.createElement("i", {className: "material-icons"}, "add")
+          )
+        )
+      );
+    }
+  });
+
+  React.render(
+    React.createElement(LogBox, null),
+    document.getElementById('react-logbox')
+  );
+  React.render(
+    React.createElement(Nav, null),
+    document.getElementById('react-nav')
+  );
+  React.render(
+    React.createElement(MainInput, null),
+    document.getElementById('react-main-input')
+  );
+  componentHandler.upgradeAllRegistered();
 });
--- renderer/jsx/components.jsx
+++ renderer/jsx/components.jsx
@@ -4,7 +4,6 @@ define(function (require) {
     _ = require('underscore'),
     yaml = require('yaml');
 
-  var LOG_LINE_HOLDER_ID = 'log-holder';
 
   var LogLine = React.createClass({
     render: function () {
@@ -18,10 +17,32 @@ define(function (require) {
   });
 
   var LogBox = React.createClass({
+    componentDidMount: function () {
+      // //actually load data
+      // var file = ipc.sendSync('load-file', {
+      //   'directory': localStorage.directory,
+      //   'file': localStorage.file
+      // });
+    },
+    getInitialState: function() {
+      // this should be empty. mocking out data for development.
+      return {
+        data: {
+          lines: [{
+            line: "This is a line",
+            timestamp: "This is a timestamp"
+          },
+          {
+            line: "This is a line",
+            timestamp: "This is a timestamp"
+          }]
+        }
+      };
+    },
     render: function () {
       return (
         <div>
-          {this.props.data.lines.map(function(line) {
+          {this.state.data.lines.map(function(line) {
             return <LogLine data={line}/>;
           })}
         </div>
@@ -29,19 +50,72 @@ define(function (require) {
     }
   });
 
-  var data = {
-    lines: [{
-      line: "This is a line",
-      timestamp: "This is a timestamp"
-    },
-    {
-      line: "This is a line",
-      timestamp: "This is a timestamp"
-    }]
-  };
-  React.render(
-    <LogBox data={data} />,
-    document.getElementById(LOG_LINE_HOLDER_ID)
-  );
+  var Instructions = React.createClass({
+    render: function () {
+      return (
+        <div className="mdl-grid hidden">
+          <div className="mdl-cell mdl-cell--12-col">
+            <h5>Where would you like to store the logs?</h5>
+            <button className="mdl-button mdl-js-button mdl-button--raised">Choose Directory</button>
+          </div>
+        </div>
+      );
+    }
+  });
 
+  var MainInput = React.createClass({
+    render: function () {
+      return (
+        <div className="mdl-textfield mdl-js-textfield">
+          <textarea className="mdl-textfield__input main-input" type="text" rows= "4"></textarea>
+          <label className="mdl-textfield__label" for="main-input">Log here...</label>
+        </div>
+      );
+    }
+  });
+
+  var FileName = React.createClass({
+    render: function () {
+      return (
+        <span className="mdl-layout-title filename" id="filename" contentEditable="true">untitlted_log_file</span>
+      );
+    }
+  });
+
+  var Nav = React.createClass({
+    render: function () {
+      var logoPath = path.join(__dirname, 'img', 'icon.png');
+      return (
+        <div className="mdl-layout__header-row menu">
+          <span><img className="logo" src={logoPath} /></span>
+          <FileName />
+          <i className="material-icons warning hidden" id="warning">warning</i>
+          <div className="mdl-layout-spacer"></div>
+          <div id="notification" className="notification hidden"></div>
+          <div className="mdl-layout-spacer"></div>
+          <span className="hidden directory" id="directory"></span>
+          <button className="mdl-button mdl-js-button mdl-button--icon" id="change-directory">
+            <i className="material-icons">folder_open</i>
+          </button>
+          <button className="mdl-button mdl-js-button mdl-button--icon" id="new">
+            <i className="material-icons">add</i>
+          </button>
+        </div>
+      );
+    }
+  });
+
+  React.render(
+    <LogBox />,
+    document.getElementById('react-logbox')
+  );
+  React.render(
+    <Nav />,
+    document.getElementById('react-nav')
+  );
+  React.render(
+    <MainInput />,
+    document.getElementById('react-main-input')
+  );
+  componentHandler.upgradeAllRegistered();
 });
