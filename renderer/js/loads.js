6a36e59010fc4426a19141c5abdc7fc4130af6af
Ben Vogt <benjvogt@gmail.com>
2015-09-20 14:43:57 -0500

    removing unused libraries

--- renderer/js/loads.js
+++ renderer/js/loads.js
@@ -2,8 +2,6 @@ requirejs.config({
     baseUrl: 'js',
     paths: {
       components: 'components',
-      jquery: 'jquery-2.1.4.min',
-      handlebars: 'handlebars-v3.0.3',
       react: 'react.min',
       moment: 'moment',
       underscore: 'underscore.min',
@@ -11,24 +9,20 @@ requirejs.config({
       highlight: 'highlight.pack'
     },
     shim: {
-      'jquery': 'jquery-2.1.4.min',
       'react': 'react.min',
       'highlight': 'highlight.pack',
       'moment': 'moment',
       'underscore': 'underscore.min',
       'yaml': 'js-yaml',
-      'handlebars': 'handlebars-v3.0.3',
       components: {
-        deps: ['jquery', 'react', 'yaml', 'moment', 'underscore', 'highlight']
+        deps: ['react', 'yaml', 'moment', 'underscore', 'highlight']
       }
     }
 });
 
 requirejs([
-  'jquery',
   'react',
   'highlight',
-  'handlebars',
   'underscore',
   'yaml',
   'moment',
